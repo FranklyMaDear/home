f2e96edabebce12005b1e8af10774759bb63fa79
FMD <Info.franklydear@gmail.com>
2025-11-26 13:44:42 +0200

    Add service worker for caching resources

---
+++ cafedium/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'cafedium-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/panel.html',
+  '/coffeetime.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
